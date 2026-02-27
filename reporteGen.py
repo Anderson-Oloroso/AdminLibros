84aa504f5c448f3c74da0abc09c0c6dc240a79a5
Anderson-Oloroso <henrikog4@gmail.com>
2026-02-27 09:31:37 -0600

    Correccion

--- reporteGen.py
+++ reporteGen.py
@@ -3,33 +3,29 @@ from menu import separador
 
 def reporte_por_genero():
     separador()
+    generos = []
     with open("libros.json", "r") as file:
         datos = json.load(file)
+        for _ in datos:
+            print(f"{_['Genero']}")
+            if _['Genero'] not in generos:
+                generos.append(_['Genero'])
+        cont = 1
+        while cont != len(generos):
+            for _ in generos:
+                print(_) 
+            cont += 1
         
-        elemntos = len(datos)
         separador()
-        for dato in datos:
-            if dato['Genero']:
-                print(dato['Genero'])
-                print(f"\nTitulo: {dato['Titulo']}\nAutor: {dato['Autor']}\nValoracion: {dato['Valoracion']}\n")
 
     with open("musicas.json", "r") as file2:
         datos2 = json.load(file2)
-        elemntos = len(datos2)
-        print(elemntos)
+        for _ in datos2:
+            print(f"{_['Genero']}")
         separador()
-        for dato in datos:
-            if dato['Genero']:
-                print(dato['Genero'])
-                print(f"\nTitulo: {dato['Titulo']}\nAutor: {dato['Autor']}\nValoracion: {dato['Valoracion']}\n")
 
     with open("peliculas.json", "r") as file3:
         datos3 = json.load(file3)
-        print(elemntos)
-        print("Elementos",elemntos)
+        for _ in datos3:
+            print(f"{_['Genero']}")
         separador()
-        for dato in datos:
-            elemntos = len(datos3)
-            if dato['Genero']:
-                print(dato['Genero'])
-                print(f"\nTitulo: {dato['Titulo']}\nAutor: {dato['Autor']}\nValoracion: {dato['Valoracion']}\n")
